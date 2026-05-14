6b6b0a2e6ac8745d5ce068d416f6a0aa59359c63
ValdisEizans <valdis.eizans@gmail.com>
2026-05-14 11:47:13 +0300

    Edit: vertice node created

---
+++ src/datastr/MyVerticeNode.java
@@ -0,0 +1,43 @@
+package datastr;
+
+public class MyVerticeNode<Ttype> {
+	private Ttype verticeElement;
+	private MyEdgeNode firstEdgeNode = null;
+	
+	public Ttype getVerticeElement() {
+		return verticeElement;
+	}
+	public MyEdgeNode getFirstEdgeNode() {
+		return firstEdgeNode;
+	}
+
+	public void setVerticeElement(Ttype verticeElement) {
+		if(verticeElement != null) {
+			this.verticeElement = verticeElement;
+		}
+		else {
+			this.verticeElement = (Ttype) new Object();
+		}
+	}
+	public void setFirstEdgeNode(MyEdgeNode firstEdgeNode) {
+		this.firstEdgeNode = firstEdgeNode;
+	}
+	
+	public MyVerticeNode(Ttype verticeElement) {
+		setVerticeElement(verticeElement);
+	}
+	
+	public String toString() {
+		String result = verticeElement + " => ";
+		MyEdgeNode currentEdgeNode = firstEdgeNode;
+		
+		while(currentEdgeNode != null) {
+			result += currentEdgeNode.toString();//toString var neizsaukt jo bus automatiski
+			result += " => ";
+			currentEdgeNode = currentEdgeNode.getNextEdge();
+		}
+		
+		return result;
+	}
+	
+}
